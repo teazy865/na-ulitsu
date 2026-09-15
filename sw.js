824dcee22eb78065db8ecb83dc833c5337e1abc6
teazy865 <47463363+teazy865@users.noreply.github.com>
2026-09-15 18:38:31 +0300

    Clickable daily forecast with morning day evening night

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("naulitsu-v2").then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest", "./icon.svg"])));
+  e.waitUntil(caches.open("naulitsu-v3").then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest", "./icon.svg"])));
   self.skipWaiting();
 });
 self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== "naulitsu-v2").map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== "naulitsu-v3").map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", (e) => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
