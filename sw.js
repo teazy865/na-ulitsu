8dc8e660cfa88bb8aba32faea31418f57b1125e4
teazy865 <47463363+teazy865@users.noreply.github.com>
2026-09-15 19:13:59 +0300

    Disable SW cache so design updates show immediately

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("naulitsu-v7").then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest", "./icon.svg"])));
-  self.skipWaiting();
-});
+self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== "naulitsu-v7").map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request, { cache: "no-store" }).catch(() => caches.match(e.request)));
 });
