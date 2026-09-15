681db1eb05b45a2b41fe1b30d0689342c5c86454
teazy865 <47463363+teazy865@users.noreply.github.com>
2026-09-15 17:47:39 +0300

    Bump cache for consensus forecast

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("naulitsu-v1").then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest", "./icon.svg"])));
+  e.waitUntil(caches.open("naulitsu-v2").then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest", "./icon.svg"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== "naulitsu-v2").map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
