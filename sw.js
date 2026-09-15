1ab7ac55c492d116bad0fb7b96646d1363fe4507
teazy865 <47463363+teazy865@users.noreply.github.com>
2026-09-15 19:05:26 +0300

    Cache v6

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("naulitsu-v5").then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest", "./icon.svg"])));
+  e.waitUntil(caches.open("naulitsu-v6").then((c) => c.addAll(["./", "./index.html", "./manifest.webmanifest", "./icon.svg"])));
   self.skipWaiting();
 });
 self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== "naulitsu-v5").map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== "naulitsu-v6").map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", (e) => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
